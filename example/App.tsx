1657cfc36b32fc3ad14e468e28b2671d3b41be98
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-11-16 15:46:33 +0100

    New App.tsx

--- example/App.tsx
+++ example/App.tsx
@@ -1,68 +1,134 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {useState} from 'react';
 import {
+  CenteredControlBar,
+  CenteredDelayedActivityIndicator,
+  ControlBar,
+  DEFAULT_THEOPLAYER_THEME,
+  FullscreenButton,
+  LanguageMenuButton,
+  MuteButton,
+  PipButton,
+  PlaybackRateSubMenu,
+  PlayButton,
+  QualitySubMenu,
+  SettingsMenuButton,
+  SkipButton,
+  Spacer,
+  TimeLabel,
+  UiContainer,
+} from '@theoplayer/react-native-ui';
+import {
+  PlayerConfiguration,
   PlayerEventType,
   THEOplayer,
   THEOplayerView,
 } from 'react-native-theoplayer';
 
-const playerConfig = {
+import {Platform, StyleSheet, View, ViewStyle} from 'react-native';
+
+const playerConfig: PlayerConfiguration = {
+  // Get your THEOplayer license from https://portal.theoplayer.com/
+  // Without a license, only demo sources hosted on '*.theoplayer.com' domains can be played.
   license: 'YOUR_LICENSE_HERE',
-};
-const onPlayerReady = (player: THEOplayer) => {
-  // optional debug logs
-  player.addEventListener(PlayerEventType.SOURCE_CHANGE, console.log);
-  player.addEventListener(PlayerEventType.LOADED_DATA, console.log);
-  player.addEventListener(PlayerEventType.LOADED_METADATA, console.log);
-  player.addEventListener(PlayerEventType.READYSTATE_CHANGE, console.log);
-  player.addEventListener(PlayerEventType.PLAY, console.log);
-  player.addEventListener(PlayerEventType.PLAYING, console.log);
-  player.addEventListener(PlayerEventType.PAUSE, console.log);
-  player.addEventListener(PlayerEventType.SEEKING, console.log);
-  player.addEventListener(PlayerEventType.SEEKED, console.log);
-  player.addEventListener(PlayerEventType.ENDED, console.log);
-  player.source = {
-    sources: {
-      src: 'https://cdn.theoplayer.com/video/adultswim/clip.m3u8',
-      type: 'application/x-mpegurl',
-    },
-    poster: 'https://cdn.theoplayer.com/video/adultswim/poster.jpg',
-    metadata: {
-      title: 'The Venture Bros',
-      subtitle: 'Adult Swim',
-      album: 'React-Native THEOplayer demos',
-      displayIconUri: 'https://cdn.theoplayer.com/video/adultswim/poster.jpg',
-      artist: 'THEOplayer',
-      type: 'tv-show',
-      releaseDate: 'november 29th',
-      releaseYear: 1997,
-    },
-  };
-  player.muted = true;
-  player.autoplay = true;
-
-  player.backgroundAudioConfiguration = {enabled: true};
-  player.pipConfiguration = {startsAutomatically: true};
-  console.log('THEOplayer is ready:', player.version);
+  chromeless: true,
+  libraryLocation: 'theoplayer',
+  mediaControl: {
+    mediaSessionEnabled: true,
+  },
+  mutedAutoplay: 'all',
 };
 
+/**
+ * The example app demonstrates the use of the THEOplayerView with a custom UI using the provided UI components.
+ * If you don't want to create a custom UI, you can just use the THEOplayerDefaultUi component instead.
+ */
 export default function App() {
+  const [player, setPlayer] = useState<THEOplayer | undefined>(undefined);
+  const chromeless = playerConfig?.chromeless ?? false;
+  const onPlayerReady = (player: THEOplayer) => {
+    setPlayer(player);
+    // optional debug logs
+    player.addEventListener(PlayerEventType.SOURCE_CHANGE, console.log);
+    player.addEventListener(PlayerEventType.LOADED_DATA, console.log);
+    player.addEventListener(PlayerEventType.LOADED_METADATA, console.log);
+    player.addEventListener(PlayerEventType.READYSTATE_CHANGE, console.log);
+    player.addEventListener(PlayerEventType.PLAY, console.log);
+    player.addEventListener(PlayerEventType.PLAYING, console.log);
+    player.addEventListener(PlayerEventType.PAUSE, console.log);
+    player.addEventListener(PlayerEventType.SEEKING, console.log);
+    player.addEventListener(PlayerEventType.SEEKED, console.log);
+    player.addEventListener(PlayerEventType.ENDED, console.log);
+    player.source = {
+      sources: {
+        src: 'https://cdn.theoplayer.com/video/adultswim/clip.m3u8',
+        type: 'application/x-mpegurl',
+      },
+    };
+
+    player.muted = true;
+    player.autoplay = true;
+
+    player.backgroundAudioConfiguration = {enabled: true};
+    player.pipConfiguration = {startsAutomatically: true};
+    console.log('THEOplayer is ready:', player.version);
+  };
+
+  const needsBorder = Platform.OS === 'ios';
+  const PLAYER_CONTAINER_STYLE: ViewStyle = {
+    position: 'absolute',
+    top: needsBorder ? 20 : 0,
+    left: needsBorder ? 5 : 0,
+    bottom: 0,
+    right: needsBorder ? 5 : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000',
+  };
+
   return (
-    <View style={styles.container}>
-      <THEOplayerView
-        style={StyleSheet.absoluteFill}
-        config={playerConfig}
-        onPlayerReady={onPlayerReady}
-      />
+    <View style={[StyleSheet.absoluteFill, {backgroundColor: '#000000'}]}>
+      <View style={PLAYER_CONTAINER_STYLE}>
+        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady}>
+          {player !== undefined && chromeless && (
+            <UiContainer
+              theme={{...DEFAULT_THEOPLAYER_THEME}}
+              player={player}
+              behind={<CenteredDelayedActivityIndicator size={50} />}
+              top={
+                <ControlBar>
+                  <LanguageMenuButton />
+                  <SettingsMenuButton>
+                    {/*Note: quality selection is not available on iOS */}
+                    <QualitySubMenu />
+                    <PlaybackRateSubMenu />
+                  </SettingsMenuButton>
+                </ControlBar>
+              }
+              center={
+                <CenteredControlBar
+                  left={<SkipButton skip={-10} />}
+                  middle={<PlayButton />}
+                  right={<SkipButton skip={30} />}
+                />
+              }
+              bottom={
+                <>
+                  <ControlBar style={{justifyContent: 'flex-start'}} />
+                  <ControlBar />
+                  <ControlBar>
+                    <MuteButton />
+                    <TimeLabel showDuration={true} />
+                    <Spacer />
+                    <PipButton />
+                    <FullscreenButton />
+                  </ControlBar>
+                </>
+              }
+            />
+          )}
+        </THEOplayerView>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
