1c8a6519d43e3a3fcb9f5a84eec7b75d57944d7c
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2024-02-12 13:37:13 +0100

    Improve AdDisplay

--- src/ui/components/ads/AdDisplay.tsx
+++ src/ui/components/ads/AdDisplay.tsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import { PlayerContext, UiContext } from '../util/PlayerContext';
 import { StyleProp, Text, TextStyle } from 'react-native';
 import { AdEvent, AdEventType, PlayerEventType } from 'react-native-theoplayer';
+import { arrayFind } from '../../utils/ArrayUtils';
+import { isLinearAd } from '../../utils/AdUtils';
 
 interface AdDisplayProps {
   /**
@@ -29,8 +31,12 @@ export class AdDisplay extends PureComponent<AdDisplayProps, AdDisplayState> {
   }
 
   componentDidMount() {
-    const player = (this.context as UiContext).player;
+    const context = this.context as UiContext;
+    const player = context.player;
     player.addEventListener(PlayerEventType.AD_EVENT, this.onAdEvent);
+    if (context.adInProgress) {
+      void this.updateAdDisplayState();
+    }
   }
 
   componentWillUnmount() {
@@ -42,21 +48,25 @@ export class AdDisplay extends PureComponent<AdDisplayProps, AdDisplayState> {
     void this.updateAdDisplayState(event.subType);
   };
 
-  private async updateAdDisplayState(type: AdEventType) {
-    const { adPlaying, currentAd } = this.state;
-
-    if (type === AdEventType.AD_BREAK_BEGIN && !adPlaying) {
-      const player = (this.context as UiContext).player;
-      const currentAdBreak = await player.ads.currentAdBreak();
-      const currentAds = currentAdBreak.ads;
-      if (currentAds) {
-        this.setState({ adPlaying: true, currentAd: 0, totalAds: currentAds.length });
-      }
-    } else if (type === AdEventType.AD_BREAK_END) {
+  private async updateAdDisplayState(type?: AdEventType): Promise<void> {
+    const { adPlaying } = this.state;
+    const context = this.context as UiContext;
+    if (type === AdEventType.AD_BREAK_END) {
       this.setState(AdDisplay.initialState);
-    } else if (type === AdEventType.AD_BEGIN) {
-      const newCurrentAd = currentAd ? currentAd + 1 : 1;
-      this.setState({ currentAd: newCurrentAd });
+    } else if (adPlaying || context.adInProgress || type === AdEventType.AD_BREAK_BEGIN) {
+      const player = context.player;
+      const currentAdBreak = await player.ads.currentAdBreak();
+      const currentLinearAds = (currentAdBreak.ads ?? []).filter(isLinearAd);
+      let index = 0;
+      if (currentLinearAds.length > 1) {
+        const currentAds = await player.ads.currentAds();
+        const currentLinearAd = arrayFind(currentAds, isLinearAd);
+        if (currentLinearAd) {
+          index = currentLinearAds.indexOf(currentLinearAd);
+        }
+      }
+      this.setState({ adPlaying: true, currentAd: index + 1, totalAds: currentLinearAds.length });
+      return;
     }
   }
 
