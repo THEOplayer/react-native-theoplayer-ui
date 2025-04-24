69f5bb8447ef669ddf669594a4893a9b6d419e30
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-04-24 12:03:41 +0200

    Rework useTVEventHandler

--- src/ui/components/util/TVUtils.ts
+++ src/ui/components/util/TVUtils.ts
@@ -2,16 +2,15 @@ import { Platform } from 'react-native';
 import * as ReactNative from 'react-native';
 
 export let useTVEventHandler: typeof ReactNative.useTVEventHandler = (handler) => {
-  if (Platform.OS === 'ios' && Platform.isTV) {
-    try {
-      useTVEventHandler = ReactNative.useTVEventHandler;
-      return useTVEventHandler(handler);
-    } catch {
-      console.warn('useTVEventHandler not supported, a dependency on react-native-tvos is required.');
-    }
-  }
   useTVEventHandler = (_handler) => {
     // Do nothing.
   };
+  if (Platform.OS === 'ios' && Platform.isTV) {
+    if (typeof ReactNative.useTVEventHandler === 'function') {
+      useTVEventHandler = ReactNative.useTVEventHandler;
+    } else {
+      console.warn('useTVEventHandler not supported, a dependency on react-native-tvos is required.');
+    }
+  }
   return useTVEventHandler(handler);
 };
