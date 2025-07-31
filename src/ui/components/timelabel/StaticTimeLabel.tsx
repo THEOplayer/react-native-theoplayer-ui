5b70b19eaf425222f67860c4845d86c842c140b9
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-07-31 12:43:21 +0200

    Clean StaticTimeLabel component

--- src/ui/components/timelabel/StaticTimeLabel.tsx
+++ src/ui/components/timelabel/StaticTimeLabel.tsx
@@ -1,6 +1,6 @@
 import { StyleProp, Text, TextStyle } from 'react-native';
-import React from 'react';
-import { PlayerContext, UiContext } from '../util/PlayerContext';
+import React, { useContext } from 'react';
+import { PlayerContext } from '../util/PlayerContext';
 
 export interface StaticTimeLabelProps {
   /**
@@ -18,7 +18,7 @@ export interface StaticTimeLabelProps {
   /**
    * The duration of the current source.
    */
-  duration: number;
+  duration?: number;
 }
 
 /**
@@ -26,38 +26,37 @@ export interface StaticTimeLabelProps {
  */
 export function StaticTimeLabel(props: StaticTimeLabelProps) {
   const { style, showDuration, time, duration } = props;
+  const context = useContext(PlayerContext);
 
-  // An unknown duration is reported as NaN.
-  if (isNaN(duration)) {
+  // An invalid duration
+  if (showDuration && !isValidDuration(duration)) {
     return <></>;
   }
 
   // Live streams report an Infinity duration.
-  if (!isFinite(duration)) {
-    return (
-      <PlayerContext.Consumer>
-        {(context: UiContext) => (
-          <Text style={[context.style.text, { color: context.style.colors.text }, style]}>{context.locale.liveLabel}</Text>
-        )}
-      </PlayerContext.Consumer>
-    );
+  if (showDuration && isLiveDuration(duration)) {
+    return <Text style={[context.style.text, { color: context.style.colors.text }, style]}>{context.locale.liveLabel}</Text>;
   }
 
   try {
     let currentTimeLabel = new Date(time).toISOString().substring(11, 19);
-    let durationLabel = new Date(duration).toISOString().substring(11, 19);
+    let durationLabel = new Date(duration ?? 0).toISOString().substring(11, 19);
     if (durationLabel.startsWith('00:')) {
       // Don't render hours if not needed.
       currentTimeLabel = currentTimeLabel.slice(3);
       durationLabel = durationLabel.slice(3);
     }
     const label = showDuration ? `${currentTimeLabel} / ${durationLabel}` : currentTimeLabel;
-    return (
-      <PlayerContext.Consumer>
-        {(context: UiContext) => <Text style={[context.style.text, { color: context.style.colors.text }, style]}>{label}</Text>}
-      </PlayerContext.Consumer>
-    );
+    return <Text style={[context.style.text, { color: context.style.colors.text }, style]}>{label}</Text>;
   } catch {
     return <></>;
   }
+}
+
+function isValidDuration(duration: number | undefined): boolean {
+  return duration !== undefined && !isNaN(duration);
+}
+
+function isLiveDuration(duration: number | undefined): boolean {
+  return duration !== undefined && !isFinite(duration);
 }
