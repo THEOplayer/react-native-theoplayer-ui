f32b395d8775bf4fdaf1d311f0223a98a32b7616
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-07-31 17:36:02 +0200

    Collapse ActivityIndicator

--- src/ui/components/activityindicator/CenteredDelayedActivityIndicator.tsx
+++ src/ui/components/activityindicator/CenteredDelayedActivityIndicator.tsx
@@ -1,21 +1,26 @@
 import React, { useContext } from 'react';
-import { DelayedActivityIndicator, type DelayedActivityIndicatorProps } from './DelayedActivityIndicator';
-import { View } from 'react-native';
+import { ActivityIndicator, ActivityIndicatorProps, type StyleProp, View, type ViewStyle } from 'react-native';
 import { PlayerContext } from '../util/PlayerContext';
 import { FULLSCREEN_CENTER_STYLE } from '../uicontroller/UiContainer';
-import { useWaiting } from '../../hooks/useWaiting';
+import { useWaiting, WAITING_DEFAULT_DELAY_MS } from '../../hooks/useWaiting';
+
+export interface DelayedActivityIndicatorProps extends ActivityIndicatorProps {
+  containerStyle?: StyleProp<ViewStyle>;
+
+  delay?: number;
+}
 
 /**
  * The default spinner for the `react-native-theoplayer` UI. It renders an ActivityIndicator when the player's playback is stalling due
  * readystate changes.
  */
 export const CenteredDelayedActivityIndicator = (props: DelayedActivityIndicatorProps) => {
-  const waiting = useWaiting();
+  const waiting = useWaiting(props.delay ?? WAITING_DEFAULT_DELAY_MS);
   const context = useContext(PlayerContext);
 
   return waiting ? (
-    <View style={FULLSCREEN_CENTER_STYLE}>
-      <DelayedActivityIndicator color={context.style.colors.icon} {...props} />
+    <View style={[FULLSCREEN_CENTER_STYLE, props.containerStyle]}>
+      <ActivityIndicator color={context.style.colors.icon} {...props} />
     </View>
   ) : (
     <></>
