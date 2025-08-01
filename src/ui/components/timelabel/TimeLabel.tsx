dfd073f0a45c674335d0c6177654cded012003f2
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-08-01 15:41:09 +0200

    Refactor TimeLabel to functional component

--- src/ui/components/timelabel/TimeLabel.tsx
+++ src/ui/components/timelabel/TimeLabel.tsx
@@ -1,8 +1,7 @@
 import type { StyleProp, TextStyle } from 'react-native';
-import React, { PureComponent } from 'react';
-import { DurationChangeEvent, PlayerEventType, TimeUpdateEvent } from 'react-native-theoplayer';
-import { PlayerContext, UiContext } from '../util/PlayerContext';
+import React from 'react';
 import { StaticTimeLabel } from './StaticTimeLabel';
+import { useCurrentTime, useDuration } from '../../hooks/barrel';
 
 export interface TimeLabelProps {
   /**
@@ -13,11 +12,6 @@ export interface TimeLabelProps {
    * The style overrides.
    */
   style?: StyleProp<TextStyle>;
-}
-
-export interface TimeLabelState {
-  currentTime: number;
-  duration: number;
 }
 
 /**
@@ -33,47 +27,9 @@ export const DEFAULT_TIME_LABEL_STYLE: TextStyle = {
 /**
  * The default time label component for the `react-native-theoplayer` UI.
  */
-export class TimeLabel extends PureComponent<TimeLabelProps, TimeLabelState> {
-  private static initialState: TimeLabelState = {
-    currentTime: 0.0,
-    duration: 0,
-  };
-
-  constructor(props: TimeLabelProps) {
-    super(props);
-    this.state = TimeLabel.initialState;
-  }
-
-  componentDidMount() {
-    const player = (this.context as UiContext).player;
-    player.addEventListener(PlayerEventType.TIME_UPDATE, this.onTimeUpdate);
-    player.addEventListener(PlayerEventType.DURATION_CHANGE, this.onDurationChange);
-    this.setState({ currentTime: player.currentTime, duration: player.duration });
-  }
-
-  componentWillUnmount() {
-    const player = (this.context as UiContext).player;
-    player.removeEventListener(PlayerEventType.TIME_UPDATE, this.onTimeUpdate);
-    player.removeEventListener(PlayerEventType.DURATION_CHANGE, this.onDurationChange);
-  }
-
-  private onTimeUpdate = (event: TimeUpdateEvent) => {
-    const { currentTime } = event;
-    this.setState({ currentTime });
-  };
-
-  private onDurationChange = (event: DurationChangeEvent) => {
-    const { duration } = event;
-    this.setState({ duration });
-  };
-
-  render() {
-    const { currentTime, duration } = this.state;
-    const { style, showDuration } = this.props;
-    return (
-      <StaticTimeLabel showDuration={showDuration} time={currentTime} duration={duration} style={[DEFAULT_TIME_LABEL_STYLE, style]}></StaticTimeLabel>
-    );
-  }
-}
-
-TimeLabel.contextType = PlayerContext;
+export const TimeLabel = (props: TimeLabelProps) => {
+  const currentTime = useCurrentTime();
+  const duration = useDuration();
+  const { showDuration, style } = props;
+  return <StaticTimeLabel showDuration={showDuration} time={currentTime} duration={duration} style={[DEFAULT_TIME_LABEL_STYLE, style]} />;
+};
