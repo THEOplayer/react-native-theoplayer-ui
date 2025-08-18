465a55e442c84db063ca922df9a060992e98b50f
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-08-18 12:02:04 +0200

    Merge scrubberTime into useSliderHook

--- src/ui/components/seekbar/useSliderTime.ts
+++ src/ui/components/seekbar/useSliderTime.ts
@@ -1,4 +1,10 @@
-import { useCurrentTime } from '../../hooks/useCurrentTime';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { PlayerContext } from '@theoplayer/react-native-ui';
+import { type PlayerEventMap, PlayerEventType } from 'react-native-theoplayer';
+
+const TIME_CHANGE_EVENTS = [PlayerEventType.TIME_UPDATE, PlayerEventType.SEEKING, PlayerEventType.SEEKED] satisfies ReadonlyArray<
+  keyof PlayerEventMap
+>;
 
 /**
  * Returns the player's current slider time.
@@ -8,7 +14,24 @@ import { useCurrentTime } from '../../hooks/useCurrentTime';
  **
  * @group Hooks
  */
-export const useSliderTime = () => {
-  const currentTime = useCurrentTime();
-  return Number.isFinite(currentTime) ? Math.round(currentTime * 1e-3) * 1e3 : 0;
+export const useSliderTime = (): [number, boolean, React.Dispatch<React.SetStateAction<boolean>>] => {
+  const { player } = useContext(PlayerContext);
+  const [isScrubbing, setIsScrubbing] = useState(false);
+  const [currentTime, setCurrentTime] = useState(player?.currentTime ?? 0);
+  const onTimeUpdate = useCallback(() => {
+    // Block time updates while scrubbing
+    if (player && !isScrubbing) {
+      setCurrentTime(player.currentTime);
+    }
+  }, [player, isScrubbing]);
+
+  useEffect(() => {
+    if (!player) return;
+    TIME_CHANGE_EVENTS.forEach((event) => player.addEventListener(event, onTimeUpdate));
+    return () => {
+      TIME_CHANGE_EVENTS.forEach((event) => player.removeEventListener(event, onTimeUpdate));
+    };
+  }, [player, onTimeUpdate]);
+
+  return [Number.isFinite(currentTime) ? Math.round(currentTime * 1e-3) * 1e3 : 0, isScrubbing, setIsScrubbing];
 };
