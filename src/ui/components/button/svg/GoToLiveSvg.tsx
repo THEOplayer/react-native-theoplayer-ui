5d1bf2575296fd91c9b88e60c3be08533cf8e99a
rbnbtns <ruben.baetens@theoplayer.com>
2025-10-24 17:47:24 +0200

    Bring SVG more in line with others

--- src/ui/components/button/svg/GoToLiveSvg.tsx
+++ src/ui/components/button/svg/GoToLiveSvg.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Svg, Path, Line } from 'react-native-svg';
+import { Svg, Path, Line, type SvgProps } from 'react-native-svg';
+import { SvgContext } from '@theoplayer/react-native-ui';
 
-export function GoToLiveSvg({ size = 24, color = '#fff' }) {
+export function GoToLiveSvg(props: SvgProps) {
   return (
-    <Svg width={size} height={size} viewBox="0 0 24 24">
-      <Path d="M4 4 L16 12 L4 20 Z" fill={color} />
-      <Line x1="19" y1="4" x2="19" y2="20" stroke={color} strokeWidth="2" strokeLinecap="round" />
-    </Svg>
+    <SvgContext.Consumer>
+      {(context) => (
+        <>
+          <Svg viewBox={'0 0 24 24'} {...context} {...props}>
+            <Path d="M4 4 L16 12 L4 20 Z" />
+            <Line x1="19" y1="5" x2="19" y2="19" strokeWidth="2" strokeLinecap="square" stroke={'#fff'} />
+          </Svg>
+        </>
+      )}
+    </SvgContext.Consumer>
   );
 }
