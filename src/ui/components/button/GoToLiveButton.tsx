36382dadac5a5aca7d5871d3d6936c4ac429c426
rbnbtns <ruben.baetens@theoplayer.com>
2025-10-25 19:38:35 +0200

    Hide when playing VOD

--- src/ui/components/button/GoToLiveButton.tsx
+++ src/ui/components/button/GoToLiveButton.tsx
@@ -1,6 +1,6 @@
 import type { ButtonBaseProps } from './ButtonBaseProps';
 import React, { type ReactNode, useCallback, useContext } from 'react';
-import { ActionButton, PlayerContext } from '@theoplayer/react-native-ui';
+import { ActionButton, PlayerContext, useDuration } from '@theoplayer/react-native-ui';
 import { TestIDs } from '../../utils/TestIDs';
 import { GoToLiveSvg } from './svg/GoToLiveSvg';
 
@@ -18,10 +18,15 @@ export function GoToLiveButton(props: LiveButtonProps) {
   const { icon, style } = props;
   const goLiveSvg: ReactNode = icon?.goToLive ?? <GoToLiveSvg />;
   const { player } = useContext(PlayerContext);
+  const duration = useDuration();
 
   const goToLive = useCallback(() => {
     player.currentTime = Infinity;
   }, [player]);
+
+  if (duration !== Infinity) {
+    return <></>;
+  }
 
   return <ActionButton style={style} testID={props.testID ?? TestIDs.GO_TO_LIVE_BUTTON} touchable={true} svg={goLiveSvg} onPress={goToLive} />;
 }
