7a9fcb8c0326bb93e414d43bc8169acc3a282e09
rbnbtns <ruben.baetens@theoplayer.com>
2025-10-27 15:42:39 +0100

    Use correct style overrides

--- src/ui/components/button/GoToLiveButton.tsx
+++ src/ui/components/button/GoToLiveButton.tsx
@@ -4,7 +4,7 @@ import { PlayerContext } from '../util/PlayerContext';
 import { ActionButton } from './actionbutton/ActionButton';
 import { useCurrentTime, useDuration, useSeekable } from '../../hooks/barrel';
 import { isAtLive, isLiveDuration } from '../util/LiveUtils';
-import { Text, View } from 'react-native';
+import { StyleProp, Text, TextStyle, View } from 'react-native';
 import { TestIDs } from '../../utils/TestIDs';
 
 export interface LiveButtonProps extends ButtonBaseProps {
@@ -12,13 +12,17 @@ export interface LiveButtonProps extends ButtonBaseProps {
    * The icon components used in the button.
    */
   icon?: { goToLive: ReactNode };
+  /**
+   * The text style overrides for the button.
+   */
+  textStyle?: StyleProp<TextStyle>;
 }
 
 /**
  * The default go to live button for the `react-native-theoplayer` UI.
  */
 export function GoToLiveButton(props: LiveButtonProps) {
-  const { style } = props;
+  const { style, textStyle } = props;
   const context = useContext(PlayerContext);
   const duration = useDuration();
   const currentTime = useCurrentTime();
@@ -36,13 +40,13 @@ export function GoToLiveButton(props: LiveButtonProps) {
 
   return (
     <ActionButton
-      style={[props.style, { justifyContent: 'center', aspectRatio: 1.5 }]}
+      style={[props.style, { justifyContent: 'center', aspectRatio: 1.5 }, style]}
       testID={props.testID ?? TestIDs.GO_TO_LIVE_BUTTON}
       onPress={goToLive}
       touchable={true}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={[context.style.text, { color: liveIndicatorColor, marginRight: 6 }, style]}>●</Text>
-        <Text style={[context.style.text, { color: context.style.colors.text }, style]}>{context.locale.liveLabel}</Text>
+        <Text style={[context.style.text, { color: liveIndicatorColor, marginRight: 6 }]}>●</Text>
+        <Text style={[context.style.text, { color: context.style.colors.text }, textStyle]}>{context.locale.liveLabel}</Text>
       </View>
     </ActionButton>
   );
